387dcf30ae65bdd61bbbadb294474d9f2fdf9221
Relia <>
2019-08-12 12:02:09 +0700

    custom RMM style

--- src/calendar/day/period/index.js
+++ src/calendar/day/period/index.js
@@ -1,21 +1,18 @@
-import _ from 'lodash';
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import {
-  TouchableWithoutFeedback,
-  Text,
-  View} from 'react-native';
-import {shouldUpdate} from '../../../component-updater';
+import _ from "lodash";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { TouchableWithoutFeedback, Text, View } from "react-native";
+import { shouldUpdate } from "../../../component-updater";
 
-import * as defaultStyle from '../../../style';
-import styleConstructor from './style';
+import * as defaultStyle from "../../../style";
+import styleConstructor from "./style";
 
 class Day extends Component {
-  static displayName = 'IGNORE';
-  
+  static displayName = "IGNORE";
+
   static propTypes = {
     // TODO: selected + disabled props should be removed
-    state: PropTypes.oneOf(['selected', 'disabled', 'today', '']),
+    state: PropTypes.oneOf(["selected", "disabled", "today", ""]),
 
     // Specify theme properties to override specific styles for calendar parts. Default = {}
     theme: PropTypes.object,
@@ -25,12 +22,12 @@ class Day extends Component {
     onLongPress: PropTypes.func,
     date: PropTypes.object,
 
-    markingExists: PropTypes.bool,
+    markingExists: PropTypes.bool
   };
 
   constructor(props) {
     super(props);
-    this.theme = {...defaultStyle, ...(props.theme || {})};
+    this.theme = { ...defaultStyle, ...(props.theme || {}) };
     this.style = styleConstructor(props.theme);
     this.markingStyle = this.getDrawingStyle(props.marking || []);
     this.onDayPress = this.onDayPress.bind(this);
@@ -53,11 +50,11 @@ class Day extends Component {
       return true;
     }
 
-    return shouldUpdate(this.props, nextProps, ['state', 'children', 'onPress', 'onLongPress']);
+    return shouldUpdate(this.props, nextProps, ["state", "children", "onPress", "onLongPress"]);
   }
 
   getDrawingStyle(marking) {
-    const defaultStyle = {textStyle: {}};
+    const defaultStyle = { textStyle: {} };
     if (!marking) {
       return defaultStyle;
     }
@@ -66,28 +63,32 @@ class Day extends Component {
     } else if (marking.selected) {
       defaultStyle.textStyle.color = this.theme.selectedDayTextColor;
     }
-    const resultStyle = ([marking]).reduce((prev, next) => {
+    const resultStyle = [marking].reduce((prev, next) => {
+      console.log("**** prev", prev);
+      console.log("**** next", next);
+
       if (next.quickAction) {
         if (next.first || next.last) {
           prev.containerStyle = this.style.firstQuickAction;
           prev.textStyle = this.style.firstQuickActionText;
           if (next.endSelected && next.first && !next.last) {
-            prev.rightFillerStyle = '#c1e4fe';
+            prev.rightFillerStyle = "#c1e4fe";
           } else if (next.endSelected && next.last && !next.first) {
-            prev.leftFillerStyle = '#c1e4fe';
+            prev.leftFillerStyle = "#c1e4fe";
           }
         } else if (!next.endSelected) {
           prev.containerStyle = this.style.quickAction;
           prev.textStyle = this.style.quickActionText;
         } else if (next.endSelected) {
-          prev.leftFillerStyle = '#c1e4fe';
-          prev.rightFillerStyle = '#c1e4fe';
+          prev.leftFillerStyle = "#c1e4fe";
+          prev.rightFillerStyle = "#c1e4fe";
         }
         return prev;
       }
 
       const color = next.color;
-      if (next.status === 'NotAvailable') {
+
+      if (next.status === "NotAvailable") {
         prev.textStyle = this.style.naText;
       }
       if (next.startingDay) {
@@ -121,19 +122,25 @@ class Day extends Component {
     let fillerStyle = {};
     let fillers;
 
-    if (this.props.state === 'disabled') {
+    let isStartDay = false;
+    let isEndDay = false;
+
+    if (this.props.state === "disabled") {
       textStyle.push(this.style.disabledText);
-    } else if (this.props.state === 'today') {
+    } else if (this.props.state === "today") {
       containerStyle.push(this.style.today);
       textStyle.push(this.style.todayText);
     }
 
     if (this.props.marking) {
       containerStyle.push({
-        borderRadius: 17
+        borderRadius: 19
       });
 
       const flags = this.markingStyle;
+
+      console.log("**** flags", flags);
+
       if (flags.textStyle) {
         textStyle.push(flags.textStyle);
       }
@@ -152,26 +159,42 @@ class Day extends Component {
           backgroundColor: this.theme.calendarBackground
         };
         rightFillerStyle = {
-          backgroundColor: flags.startingDay.color
+          // backgroundColor: this.theme.calendarBackground
+          backgroundColor: "#FDE6EC"
         };
-        containerStyle.push({
-          backgroundColor: flags.startingDay.color
-        });
+
+        isStartDay = true;
+        if (this.props.state === "today") {
+          containerStyle.push({
+            backgroundColor: flags.startingDay.color,
+            borderWidth: 0
+          });
+        } else {
+          containerStyle.push({
+            backgroundColor: flags.startingDay.color
+          });
+        }
       } else if (flags.endingDay && !flags.startingDay) {
         rightFillerStyle = {
           backgroundColor: this.theme.calendarBackground
         };
         leftFillerStyle = {
-          backgroundColor: flags.endingDay.color
+          backgroundColor: "#FDE6EC"
+          // backgroundColor: "#E65377"
         };
         containerStyle.push({
-          backgroundColor: flags.endingDay.color
+          // backgroundColor: flags.endingDay.color,
+          borderColor: flags.endingDay.color,
+          borderWidth: 1,
+          backgroundColor: "white"
         });
+
+        isEndDay = true;
       } else if (flags.day) {
-        leftFillerStyle = {backgroundColor: flags.day.color};
-        rightFillerStyle = {backgroundColor: flags.day.color};
+        leftFillerStyle = { backgroundColor: flags.day.color ? "#FDE6EC" : flags.day.color };
+        rightFillerStyle = { backgroundColor: flags.day.color ? "#FDE6EC" : flags.day.color };
         // #177 bug
-        fillerStyle = {backgroundColor: flags.day.color};
+        fillerStyle = { backgroundColor: flags.day.color ? "#FDE6EC" : flags.day.color };
       } else if (flags.endingDay && flags.startingDay) {
         rightFillerStyle = {
           backgroundColor: this.theme.calendarBackground
@@ -184,23 +207,35 @@ class Day extends Component {
         });
       }
 
+      if (isStartDay && isEndDay) {
+      }
+
       fillers = (
         <View style={[this.style.fillers, fillerStyle]}>
-          <View style={[this.style.leftFiller, leftFillerStyle]}/>
-          <View style={[this.style.rightFiller, rightFillerStyle]}/>
+          <View style={[this.style.leftFiller, leftFillerStyle]} />
+          <View style={[this.style.rightFiller, rightFillerStyle]} />
         </View>
       );
     }
 
     return (
-      <TouchableWithoutFeedback
-        testID={this.props.testID}
-        onPress={this.onDayPress}
-        onLongPress={this.onDayLongPress}>
+      <TouchableWithoutFeedback testID={this.props.testID} onPress={this.onDayPress} onLongPress={this.onDayLongPress}>
         <View style={this.style.wrapper}>
           {fillers}
+          {isStartDay && (
+            <View style={this.style.helperTextContainer}>
+              <Text style={this.style.helperText}>Pick-Up</Text>
+            </View>
+          )}
+          {isEndDay && (
+            <View style={this.style.helperTextContainer}>
+              <Text style={this.style.helperText}>Return</Text>
+            </View>
+          )}
           <View style={containerStyle}>
-            <Text allowFontScaling={false} style={textStyle}>{String(this.props.children)}</Text>
+            <Text allowFontScaling={false} style={textStyle}>
+              {String(this.props.children)}
+            </Text>
           </View>
         </View>
       </TouchableWithoutFeedback>
